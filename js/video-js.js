3de3a128b038d2a10e5f0796ad1ef0409c079b8f
ludovicm67 <ludovicmuller1@gmail.com>
2017-02-04 22:00:58 +0100

    added wrapper

--- js/video-js.js
+++ js/video-js.js
@@ -1,13 +1,38 @@
 class VideoJS {
 
 	constructor({
-		selector = null,
 		autoplay = false,
+		selector = null,
 	}) {
-		this.player = document.querySelector(selector);
+		this.autoplay = autoplay;
+		this.selector = selector;
+
+		this.wrapped = false;
+
+		this.playerInit();
+	}
+
+	wrapPlayer() {
+		if (!this.wrapped) {
+			let player = document.querySelector(this.selector);
+			let wrapper = document.createElement('div');
+			wrapper.innerHTML = player.outerHTML;
+			wrapper.classList.add('video-js');
+			wrapper.style.height = player.offsetHeight + 'px';
+			wrapper.style.width = player.offsetWidth + 'px';
+			player.parentNode.insertBefore(wrapper, player);
+			this.player = wrapper.firstChild;
+			this.wrapper = wrapper;
+			player.remove();
+			this.wrapped = true;
+		}
+	}
+
+	playerInit() {
+		this.wrapPlayer();
 		this.player.controls = false;
-		this.player.autoplay = autoplay;
-		if(autoplay) this.player.play();
+		this.player.autoplay = this.autoplay;
+		if (this.autoplay) this.player.play();
 		console.info('video-js player initialized');
 	}
 
