a1fa33244097e140fdc3303b5b80d255c93a0676
ludovicm67 <ludovicmuller1@gmail.com>
2017-02-04 22:02:47 +0100

    added .editorconfig

--- js/video-js.js
+++ js/video-js.js
@@ -1,39 +1,39 @@
 class VideoJS {
 
-	constructor({
-		autoplay = false,
-		selector = null,
-	}) {
-		this.autoplay = autoplay;
-		this.selector = selector;
+  constructor({
+    autoplay = false,
+    selector = null,
+  }) {
+    this.autoplay = autoplay;
+    this.selector = selector;
 
-		this.wrapped = false;
+    this.wrapped = false;
 
-		this.playerInit();
-	}
+    this.playerInit();
+  }
 
-	wrapPlayer() {
-		if (!this.wrapped) {
-			let player = document.querySelector(this.selector);
-			let wrapper = document.createElement('div');
-			wrapper.innerHTML = player.outerHTML;
-			wrapper.classList.add('video-js');
-			wrapper.style.height = player.offsetHeight + 'px';
-			wrapper.style.width = player.offsetWidth + 'px';
-			player.parentNode.insertBefore(wrapper, player);
-			this.player = wrapper.firstChild;
-			this.wrapper = wrapper;
-			player.remove();
-			this.wrapped = true;
-		}
-	}
+  wrapPlayer() {
+    if (!this.wrapped) {
+      let player = document.querySelector(this.selector);
+      let wrapper = document.createElement('div');
+      wrapper.innerHTML = player.outerHTML;
+      wrapper.classList.add('video-js');
+      wrapper.style.height = player.offsetHeight + 'px';
+      wrapper.style.width = player.offsetWidth + 'px';
+      player.parentNode.insertBefore(wrapper, player);
+      this.player = wrapper.firstChild;
+      this.wrapper = wrapper;
+      player.remove();
+      this.wrapped = true;
+    }
+  }
 
-	playerInit() {
-		this.wrapPlayer();
-		this.player.controls = false;
-		this.player.autoplay = this.autoplay;
-		if (this.autoplay) this.player.play();
-		console.info('video-js player initialized');
-	}
+  playerInit() {
+    this.wrapPlayer();
+    this.player.controls = false;
+    this.player.autoplay = this.autoplay;
+    if (this.autoplay) this.player.play();
+    console.info('video-js player initialized');
+  }
 
 }
