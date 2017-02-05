bbe651e4930b9450fde90a64e3c1d81853951830
ludovicm67 <ludovicmuller1@gmail.com>
2017-02-05 16:01:32 +0100

    design + play/pause

--- js/video-js.js
+++ js/video-js.js
@@ -8,18 +8,23 @@ class VideoJS {
     this.selector = selector;
 
     this.wrapped = false;
+    this.player = null;
+    this.playPause = null;
 
     this.playerInit();
+
   }
 
   wrapPlayer() {
     if (!this.wrapped) {
       let player = document.querySelector(this.selector);
       let wrapper = document.createElement('div');
-      wrapper.innerHTML = player.outerHTML;
       wrapper.classList.add('video-js');
+      wrapper.innerHTML = player.outerHTML;
       wrapper.style.height = player.offsetHeight + 'px';
       wrapper.style.width = player.offsetWidth + 'px';
+      this.addControls(wrapper);
+      console.log(this);
       player.parentNode.insertBefore(wrapper, player);
       this.player = wrapper.firstChild;
       this.wrapper = wrapper;
@@ -28,12 +33,49 @@ class VideoJS {
     }
   }
 
+
+
+  addControls(wrapper) {
+    if(!this.wrapped) {
+      let controls = document.createElement('div');
+      controls.classList.add('video-js--controls');
+      this.playPause = document.createElement('div');
+      this.playPause.classList.add('video-js--playPause');
+      this.playPause.textContent = '▶';
+      this.playPause.addEventListener('click', () => {
+        if (this.player.paused) {
+          this.play();
+        } else {
+          this.pause();
+        }
+      }, false);
+      controls.appendChild(this.playPause);
+      wrapper.appendChild(controls);
+    }
+  }
+
   playerInit() {
     this.wrapPlayer();
     this.player.controls = false;
     this.player.autoplay = this.autoplay;
-    if (this.autoplay) this.player.play();
+    if (this.autoplay) this.play();
+    this.player.addEventListener('timeupdate', () => {
+      if (this.player.paused) {
+        this.playPause.textContent = '▶';
+      } else {
+        this.playPause.textContent = '▮▮';
+      }
+    });
     console.info('video-js player initialized');
+  }
+
+  // Controls
+  pause() {
+    this.player.pause();
+  }
+
+  play() {
+    this.player.play();
   }
 
 }
